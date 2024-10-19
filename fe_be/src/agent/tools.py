8ac4bb17caad03a4d95b5ff14e7212e0ea96d34d
Wojciech Łapacz <wojciech.lapacz02@gmail.com>
2024-10-20 01:40:24 +0200

    shit happens

--- fe_be/src/agent/tools.py
+++ fe_be/src/agent/tools.py
@@ -221,7 +221,7 @@ def get_explanation_tool() -> pd.DataFrame:
         'Value': float_values
     })
 
-    return df
+    return "explanation"
 
 
 explanation_tool: BaseTool = tool(get_explanation_tool)
